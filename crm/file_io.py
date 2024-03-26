2524695dc3ee4db6942e2a2f227d7eab266158a7
Z-L-D <zacldoyle@gmail.com>
2024-03-26 15:38:55 -0500

    -

---
+++ crm/file_io.py
@@ -0,0 +1,17 @@
+# file_io.py
+import os
+import pathlib
+import torch
+import urllib.request
+import gradio as gr
+from tqdm import tqdm
+import gc
+
+from common.file_io import generate_random_filename, models_dir_builder
+
+from modules.paths import models_path
+from modules.paths_internal import default_output_dir
+from modules import shared
+from omegaconf import OmegaConf
+
+models_dir = models_dir_builder("CRM")
